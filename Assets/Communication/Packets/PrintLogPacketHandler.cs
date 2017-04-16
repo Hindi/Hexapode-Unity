a8bc5f664a4f526f246a2925eabd58093e3fe654
Studer <v.studer@live.fr>
2017-04-16 20:09:10 +0200

    printLogPacketHandler add

---
+++ Assets/Communication/Packets/PrintLogPacketHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintLogPacket : PacketAbstract
+{
+    public string message;
+}
+
+public class PrintLogPacketHandler : IRecievedPacketHandler
+{
+    public void parsePacket(string message)
+    {
+        PrintLogPacket packet = new PrintLogPacket();
+        packet.Code = SerialCode.PRINT_LOG;
+        packet.message = message.Substring(2, message.Length - 2);
+        EventManager<PrintLogPacket>.Trigger(packet);
+    }
+}
---
+++ Assets/Utils/SerialLogger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerialLogger : MonoBehaviour
+{
+    private void Awake()
+    {
+        EventManager<PrintLogPacket>.Register(onRecievePrintPacket);
+    }
+
+    public void onRecievePrintPacket(PrintLogPacket packet)
+    {
+        Debug.Log(packet.message);
+    }
+}
